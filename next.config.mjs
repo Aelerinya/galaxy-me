fe7ed6fd02ee108acbc4ba2f16c9de42f4884c77
Lucie Philippon <lucie.philippon@proton.me>
2025-06-07 11:31:44 +0100

    /paris-ai-safety redirect

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/paris-ai-safety',
+        destination: 'https://docs.google.com/document/d/1pudXgEO4gQ_OvGl_8luk3sR2nS0VEclmfSpUDKXYUIE/edit?tab=t.0',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
